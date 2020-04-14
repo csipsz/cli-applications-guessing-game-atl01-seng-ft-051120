954aa2516d68b40d2ef211b4be2f0a9434d701f3
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 14:38:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def run_guessing_game
   number = rand(6) + 1 
   puts "Hi guessing gamer! Choose a number between 1 and 6!"
@@ -7,6 +9,7 @@ def run_guessing_game
   elsif guess.downcase == 'exit'
    puts "Goodbye!"
   elsif guess.to_i != number 
+  binding.pry
     puts `Sorry! The computer guessed #{number}.`
   end
 end
