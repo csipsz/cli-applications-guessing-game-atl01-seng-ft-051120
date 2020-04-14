99c36b8b103460972ec76d3f26497873898b6cb7
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 14:24:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   number = rand(6) + 1 
-  puts 
+  puts "Hi guessing gamer! Choose a number between 1 and 6!"
   guess = gets.chomp
   if number == guess.to_i 
     puts 'You guessed the correct number!'
