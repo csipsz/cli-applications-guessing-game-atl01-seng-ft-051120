8783d15cdd71ecf934a25d2e85ce7e9ed488132f
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 14:31:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,9 +4,9 @@ def run_guessing_game
   guess = gets.chomp
   if number == guess.to_i 
     puts 'You guessed the correct number!'
-  elsif number != guess.to_i && guess != 'exit'
+  elsif number != guess.to_i 
    puts `Sorry! The computer guessed #{number}.`
-  else
+  elsif guess == 'exit'
    puts "Goodbye!"
   end
 end
